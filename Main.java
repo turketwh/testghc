fb8b1bb5e803d46c69c232d4970f8ca2aaed4ecf
William Turkett <5298611+turketwh@users.noreply.github.com>
2024-01-05 12:05:25 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int x;
+        // Assign the variable 'x' the appropriate value here
+        System.out.println(x);
+    }
+}
